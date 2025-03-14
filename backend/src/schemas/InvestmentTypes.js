3359d7e715cb3520a8c80fe94444ec446cd30bb3
Dong Liang <dongliang1204@gmail.com>
2025-03-14 15:50:21 -0400

    finished implementing some schema

---
+++ backend/src/schemas/InvestmentTypes.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+import Distribution from './Distribution';
+
+const investmentTypeScehma = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    returnAmtOrPct: {
+        type: String,
+        enum: ["amount", "percent"],
+        required: true,
+    },
+    returnDistribution: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Distribution',
+        required: true,
+    },
+    expenseRatio: {
+        type: Number,
+        required: true,
+    },
+    incomeAmtOrPct: {
+        type: String,
+        enum: ["amount", "percent"],
+        required: true,
+    },
+    incomeDistribution: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Distribution',
+        required: true,
+    },
+    taxability: {
+        type: Boolean,
+        required: true
+    },
+});
+
+const InvestmentType = mongoose.model('InvestmentType', investmentTypeScehma);
+
+export default InvestmentType;
