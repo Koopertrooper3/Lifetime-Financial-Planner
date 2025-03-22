c69eae620ffc9577aee8c51a94740a54c55bd32a
Koopertrooper3 <cyu886116@gmail.com>
2025-03-22 13:01:48 -0400

    Simulation income events done

--- backend/simulator/simulationHead.ts
+++ backend/simulator/simulationHead.ts
@@ -5,8 +5,8 @@ import path from 'path';
 
 
 const connection = new IORedis({ maxRetriesPerRequest: null });
-const MAX_THREADS = 2
-const TOTAL_NUMBER_OF_SIMULATIONS = 4
+const MAX_THREADS = Number(process.env.THREADS_PER_ATTEMPT) || 1
+const TOTAL_NUMBER_OF_SIMULATIONS = Number(process.env.TOTAL_NUMBER_OF_SIMULATIONS) || 1
 
 const worker = new Worker("simulatorQueue",simulation,{connection});
 
@@ -16,14 +16,14 @@ interface queue {
 
 async function simulation(job: Job) {
   const jobData : queue = job.data
-  const sumulationsPerThread = TOTAL_NUMBER_OF_SIMULATIONS/MAX_THREADS
+  const simulationsPerThread = TOTAL_NUMBER_OF_SIMULATIONS/MAX_THREADS
 
 
   for(let threads = 0; threads < MAX_THREADS; threads++){
     const workerData = {
       scenarioID : jobData.scenarioID, 
       threadNumber: threads, 
-      sumulationsPerThread: sumulationsPerThread
+      sumulationsPerThread: simulationsPerThread
     }
     const workerThread = new WorkerThreads.Worker(path.resolve(__dirname, './simulationWorker.js'),{workerData : workerData})
     workerThread.on('exit',(code) => console.log(code))
