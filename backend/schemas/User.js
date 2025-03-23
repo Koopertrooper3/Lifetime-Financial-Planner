8b362502af23bd7e56f18c8c0febe8edf796de38
Dong Liang <dongliang1204@gmail.com>
2025-03-23 04:01:42 -0400

    update user schema

--- backend/schemas/User.js
+++ backend/schemas/User.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose'
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true},
+    googleId: { type: String, required: true },
+    name: { type: String, required: true },
     ownedSenarios: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Senario',
