51c9c5d4bb7aaac89d6c6d4b63b8a5ae70813c66
HunterHuangHub <hunter.huang@stonybrook.edu>
2025-03-31 17:41:47 -0400

    [Feature] Frontend: ChartsPage WIP

---
+++ frontend/src/pages/ChartsPage.tsx
@@ -0,0 +1,48 @@
+import "../stylesheets/ChartsPage.css";
+import Banner from "../components/Banner";
+import SideBar from "../components/Sidebar";
+import { useState, useEffect } from "react";
+import LoadingWheel from "../components/LoadingWheel";
+import axios from "axios";
+import { useHelperContext } from "../HelperContext";
+
+function ChartsPage() {
+  const { allScenarios } = useHelperContext();
+  const [userData, setUserData] = useState(null);
+
+  const fullBackendUrl =
+    "http://" +
+    import.meta.env.VITE_BACKEND_IP +
+    ":" +
+    import.meta.env.VITE_BACKEND_PORT;
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await axios.get(fullBackendUrl + "/user", {
+          withCredentials: true,
+        });
+        setUserData(response.data);
+      } catch (err) {
+        console.log("error fetching user data");
+      }
+    };
+    fetchUserData();
+  }, []);
+
+  return userData == null ? (
+    <LoadingWheel />
+  ) : (
+    <div>
+      <Banner />
+      <SideBar />
+      <div className="charts-container">
+        <h2>Charts (WIP)</h2>
+        {/* TODO: Insert Charts */}
+      </div>
+    </div>
+  );
+}
+
+export default ChartsPage;
+
