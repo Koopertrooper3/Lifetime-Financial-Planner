2586c875b344649a90aee805229a6789908d3ea4
Koopertrooper3 <cyu886116@gmail.com>
2025-04-09 12:26:52 -0400

    Simulator now returns the proper number of completed simulations

--- backend/simulator/simulationWorker.ts
+++ backend/simulator/simulationWorker.ts
@@ -93,19 +93,19 @@ async function simulation(threadData : threadData){
             for(const incomeEvent of incomeEvents){
                 if(incomeEvent.event.type == "Income"){
 
-                    const eventIncome = incomeEvent.event.initalAmount
+                    const eventIncome = incomeEvent.event.initialAmount
 
                     cash.value += eventIncome
                     currentYearIncome += eventIncome
                     pushToLog(logStream,logMessage(threadNumber,incomeEventLogMessage(simulatedYear,incomeEvent.name,eventIncome)))
 
                     if(incomeEvent.event.socialSecurity == true){
-                        currentYearSocialSecurityIncome += incomeEvent.event.initalAmount
+                        currentYearSocialSecurityIncome += incomeEvent.event.initialAmount
                     }
 
                     //Inflation adjustment
                     if(incomeEvent.event.inflationAdjusted == true){
-                        incomeEvent.event.initalAmount += incomeEvent.event.initalAmount * inflationRate
+                        incomeEvent.event.initialAmount += incomeEvent.event.initialAmount * inflationRate
                     }
 
                     
@@ -133,7 +133,7 @@ function calculateLifeExpectancy(scenario : Scenario){
     }else if(userLifeExpectancy.type == "Normal"){
         return Math.round(rnorm(1,userLifeExpectancy.mean,userLifeExpectancy.stdev)[0])
     }else{
-        return rint(1,userLifeExpectancy.lower,userLifeExpectancy.upper, 1)[0]
+        return rint(1,userLifeExpectancy.min,userLifeExpectancy.max, 1)[0]
     }
 }
 function calculateInflation(scenario : Scenario){
@@ -143,7 +143,7 @@ function calculateInflation(scenario : Scenario){
     }else if(inflationAssumption.type == "Normal"){
         return rnorm(1,inflationAssumption.mean,inflationAssumption.stdev)[0]
     }else{
-        return rint(1,inflationAssumption.lower,inflationAssumption.upper, 1)[0]
+        return rint(1,inflationAssumption.min,inflationAssumption.max, 1)[0]
     }
 }
 
