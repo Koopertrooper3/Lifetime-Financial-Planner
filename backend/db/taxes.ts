3fec4f044e8a2edeec86472b9a45a46e9a05f8a9
Dong Liang <dongliang1204@gmail.com>
2025-04-13 23:48:12 -0400

    added stateTax schema

--- backend/db/taxes.ts
+++ backend/db/taxes.ts
@@ -4,7 +4,19 @@ export interface taxBracketType {
     rate : number,
     lowerThreshold : number,
     upperThreshold : number
-  }
+}
+export interface stateTaxBracket{
+    rate: number,
+    lower_threshold: number,
+    upper_threshold: number,
+    flat_adjustment:number
+}
+export interface StateTax{
+    state: string,
+    year: number,
+    single_income_tax: stateTaxBracket[],
+    married_income_tax: stateTaxBracket[]
+}
 export interface FederalTax {
     year: number
     singleIncomeTaxBrackets : taxBracketType[],
@@ -14,6 +26,8 @@ export interface FederalTax {
     singleCapitalGainsTaxBrackets : taxBracketType[],
     marriedcapitalGainsTaxBrackets : taxBracketType[]
 }
+
+
 export const taxBracketSchema = new Schema<taxBracketType>({
     rate : Number,
     lowerThreshold : Number,
@@ -31,6 +45,20 @@ export const federalTaxSchema = new Schema<FederalTax>({
     marriedStandardDeduction: Number,
     singleCapitalGainsTaxBrackets : [taxBracketSchema],
     marriedcapitalGainsTaxBrackets : [taxBracketSchema]
+})
+
+export const stateTaxBracketSchema = new Schema<stateTaxBracket>({
+    rate: Number,
+    lower_threshold: Number,
+    upper_threshold: Number,
+    flat_adjustment: Number
+})
+
+export const stateTaxSchema = new Schema<StateTax>({
+    state: String,
+    year: Number,
+    single_income_tax: [stateTaxBracketSchema],
+    married_income_tax: [stateTaxBracketSchema]
 })
 
 export const federalTaxModel = model('taxModel',federalTaxSchema);
