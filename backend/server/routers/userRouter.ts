d71c72267ab534c68b1226766ba76236f8ba764a
Dong Liang <dongliang1204@gmail.com>
2025-04-15 16:25:57 -0400

    finished route: editing user profile with tax info

--- backend/server/routers/userRouter.ts
+++ backend/server/routers/userRouter.ts
@@ -1,17 +1,24 @@
 import express from 'express';
 import User from '../../db/User';
 import z from "zod";
+import { stateTaxZod } from '../zod/taxesZod';
 
 const router = express.Router();
 
-const userZod = z.object({
+const userCreateZod = z.object({
     googleId: z.string().min(1, "id must not be empty"),
     name: z.string().min(1, "name must not be empty")
 }).strict();
 
+const userEditZod = z.object({
+    googleId: z.string().min(1, "id must not be empty"),
+    name: z.string().min(1, "name must not be empty"),
+    stateTaxes: stateTaxZod
+}).strict();
+
 router.post("/create", async (req, res) => {
     try{
-        userZod.parse(req.body);
+        userCreateZod.parse(req.body);
         
         const newUser = await User.create({
             googleId: req.body.googleId,
@@ -28,6 +35,30 @@ router.post("/create", async (req, res) => {
     catch(err){
         console.error("Error creating user:", err);
         res.status(400).send({ message: "Error creating user", err });
+    }
+});
+
+router.post("/edit", async (req, res)=>{
+    try{
+        userEditZod.parse(req.body);
+
+        const userToEdit = await User.findOne({googleId: req.body.googleId});
+
+        if(userToEdit == null){
+            res.status(400).send({ message: `No user found with googleId: ${req.body.googleId}` });
+            return;
+        }
+
+        userToEdit.name = req.body.name;
+        userToEdit.stateTaxes = req.body.stateTaxes;
+
+        await userToEdit.save();
+
+        res.status(200).send({ message: `successfully updated user with googleId ${req.body.googleId}` });
+    }
+    catch(err){
+        console.error("Error editing user:", err);
+        res.status(400).send({ message: "Error editing user", err });
     }
 })
 
