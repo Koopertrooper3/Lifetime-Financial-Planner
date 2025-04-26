20390b031cab1aeea39ea518417248c3cf4bb25c
Koopertrooper3 <cyu886116@gmail.com>
2025-04-25 21:06:15 -0400

    Logging re exported outside worker, discretionary and non-discretionary expenses redone and logging refactored to more align with requirements

--- backend/simulator/logging.ts
+++ backend/simulator/logging.ts
@@ -19,6 +19,19 @@ export function writeCSVlog(accounts : Record<string,Investment>, writeStream : 
     writeStream.write(`${year},`+accountLogMessages.join(",")+"\n")
 }
 
+export function incomeEventLogMessage(year : number, eventName: string, amount : number){
+    return `[Income] Year: ${year}, Amount: ${amount}, Event name: ${eventName}\n`
+}
+export function RMDLogMessage(year: number, amount : number, investmentType : string){
+    return `[RMD] Year: ${year}, Amount: ${amount}, Investment Type: ${investmentType}\n`
+}
+export function expenseLogMessage(year: number, amount : number, expense : string){
+    return `[Expense]: Year: ${year}, Amount: ${amount}, Expense: ${expense}\n`
+}
+export function rothConversionLogMessage(year: number, amount : number, investmentType : string){
+    return `[Roth Conversion]: Year: ${year}, Amount ${amount}, Investment Type: ${investmentType}\n`
+}
+
 export async function closeCSVlog(writeStream : WriteStream){
     writeStream.end()
     await finished(writeStream)
