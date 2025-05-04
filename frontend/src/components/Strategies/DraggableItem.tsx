be8ee46ea41cac8ca2976f389abc90cfd5fa99bd
Dong Liang <dongliang1204@gmail.com>
2025-05-04 17:37:46 -0400

    final changes

---
+++ frontend/src/components/Strategies/DraggableItem.tsx
@@ -0,0 +1,36 @@
+// New DraggableItem component
+import { useDraggable, useDroppable } from "@dnd-kit/core";
+import { Item } from "./Types";
+
+export function DraggableItem({ item }: { item: Item }) {
+  const { attributes, listeners, setNodeRef, transform } = useDraggable({
+    id: item.id,
+    data: { type: "item" }, // Explicit item type
+  });
+
+  const { setNodeRef: setDropRef } = useDroppable({
+    id: item.id,
+    data: { type: "item" }, // Also a drop target
+  });
+
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined;
+
+  return (
+    <div
+      ref={(node) => {
+        setNodeRef(node);
+        setDropRef(node);
+      }}
+      style={style}
+      {...attributes}
+      {...listeners}
+      className="draggable-item"
+    >
+      {item.name}
+    </div>
+  );
+}
