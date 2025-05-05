6ba7135071de0e0276d00e95c066935704836c51
Koopertrooper3 <cyu886116@gmail.com>
2025-05-05 14:55:32 -0400

    Bare bones chart get route

--- backend/server/routers/chartRouter.ts
+++ backend/server/routers/chartRouter.ts
@@ -3,3 +3,20 @@ import { ProbabilityRangeChartModel } from '../../db/charts_schema/ProbabilityRa
 import { StackBarDataModel } from '../../db/charts_schema/StackBarDataSchema';
 import { SuccessProbabiltyChartModel } from '../../db/charts_schema/SuccessProbabilitySchema';
 export const router = Router();
+
+
+router.get('/:id', async (req : Request, res: Response) => {
+    const { id } = req.params;
+
+  try {
+    const probabilityRange = ProbabilityRangeChartModel.find({ chartID: id })
+    const stackBarData = StackBarDataModel.find({ chartID: id })
+    const successProbabolity = SuccessProbabiltyChartModel.find({ chartID: id })
+
+    res.status(200).json({});
+  }
+  catch (error: unknown) {
+    console.log(`Error in fetching scenarios: `, (error as Error).message);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
